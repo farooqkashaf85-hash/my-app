24889cc75b58f8179aebceaf0ef967ed74ce41c0
Kashaf Farooq <farooqkashaf85@gmail.com>
2026-08-21 12:05:47 +0500

    update cors and socket url

--- backend/backend .js
+++ backend/backend .js
@@ -8,7 +8,10 @@ const app = express();
 const server = http.createServer(app);  
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: [
+        "http://localhost:5173",
+        "https://practice-notes.netlify.app"
+    ],
         methods: ["GET", "POST" , "PUT", "DELETE"],
     },
 });
@@ -19,7 +22,10 @@ io.on("connection", (socket) => {
 }); 
 app.use(cors(
     {
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://practice-notes.netlify.app"
+    ],
     methods: ["GET", "POST" , "PUT", "DELETE"]
 }
 ));
