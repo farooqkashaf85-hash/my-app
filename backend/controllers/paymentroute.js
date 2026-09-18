a1cea8a0cb720cb8fc123fe470330ad61ea6c0c9
Kashaf Farooq <farooqkashaf85@gmail.com>
2026-09-18 11:38:20 +0500

    Fix Stripe CI tests

--- backend/controllers/paymentroute.js
+++ backend/controllers/paymentroute.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Stripe = require("stripe");
+
+const stripe = process.env.STRIPE_SECRET_KEY
+  ? new Stripe(process.env.STRIPE_SECRET_KEY)
+  : null;
 
 // Create a payment intent
 router.post('/create-payment-intent', async (req, res) => {
